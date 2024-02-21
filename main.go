62e5ecd348b165c4013f7eb51b73f9077a1add8a
AlexDev404 <alexdev404@noreply.users.github.com>
2024-02-21 13:34:17 -0600

    Added a comment above loop

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 
 import "fmt"
 
+// The main function
 func main() {
 	fmt.Println("This program demonstrates functions!")
 	// A loop
+	// Increments the value passed in by 1
 	for i := 0; i < 4; i++ {
 		fmt.Println("Hello, World!")
 	}
