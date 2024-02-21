66ff47910e448cd0e6e9db1f3fce6d82f467ffb4
AlexDev404 <alexdev404@noreply.users.github.com>
2024-02-21 12:43:33 -0600

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module alexdev404/branching
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,11 @@
+// A hello world program that prints out "Hello, World!" to the console 4 times.
+package main
+
+import "fmt"
+
+func main() {
+	// A loop
+	for i := 0; i < 4; i++ {
+		fmt.Println("Hello, World!")
+	}
+}
